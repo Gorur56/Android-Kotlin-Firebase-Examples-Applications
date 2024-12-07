19ac98f1aa02be74c3f2bd20e457b04dca287b04
Gorur56 <sevim.gorur01@gmail.com>
2024-12-07 21:42:01 +0300

    create recycle view design. and create MyAdapter and firebase struct for list

--- ToDoList/app/src/main/java/com/example/todolist/adapter/MyAdapter.kt
+++ ToDoList/app/src/main/java/com/example/todolist/adapter/MyAdapter.kt
@@ -1,4 +1,53 @@
 package com.example.todolist.adapter
 
-class MyAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.todolist.R
+import com.example.todolist.entity.DataClass
+
+class MyAdapter(private val context: Context,
+                private val dataList:List<DataClass>): RecyclerView.Adapter<MyViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.recycler_item, parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        Glide.with(context).load(dataList[position].dataImage)
+            .into(holder.recImage)
+        holder.recTitle.text = dataList[position].dataTitle
+        holder.recDesc.text = dataList[position].dataDesc
+        holder.recPriority.text = dataList[position].dataPriority
+
+        holder.recCard.setOnClickListener {
+
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
+
+}
+
+class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    var recImage: ImageView
+    var recTitle: TextView
+    var recDesc: TextView
+    var recPriority: TextView
+    var recCard: CardView
+    init {
+        recImage = itemView.findViewById(R.id.recImage)
+        recTitle = itemView.findViewById(R.id.textviewRecTitle)
+        recDesc = itemView.findViewById(R.id.textviewRecDesc)
+        recPriority = itemView.findViewById(R.id.textviewRecPriority)
+        recCard = itemView.findViewById(R.id.cardview)
+    }
 }
